a47f93f502c21efe047e891ea49ef9869caad9e7
Christopher Budo <budocf@rose-hulman.edu>
2017-01-22 02:40:27 -0500

    added path to view timeslots

--- Rendezvous/Rendezvous/views.py
+++ Rendezvous/Rendezvous/views.py
@@ -1,8 +1,8 @@
 """
 Routes and views for the flask application.
 """
-
-from datetime import datetime
+import datetime
+from datetime import datetime as dt
 from flask import render_template
 from Rendezvous import app
 
@@ -13,7 +13,7 @@ def home():
     return render_template(
         'index.html',
         title='Home Page',
-        year=datetime.now().year,
+        year=dt.now().year,
     )
 
 @app.route('/contact')
@@ -22,7 +22,7 @@ def contact():
     return render_template(
         'contact.html',
         title='Contact',
-        year=datetime.now().year,
+        year=dt.now().year,
         message='If you have any issues while using Rendezvous With Me - contact us at:'
     )
 
@@ -32,7 +32,7 @@ def plan():
     return render_template(
         'plan.html',
         title='Plan',
-        year=datetime.now().year,
+        year=dt.now().year,
         message='Your planning page.'
     )
 
@@ -42,19 +42,27 @@ def about():
     return render_template(
         'about.html',
         title='About',
-        year=datetime.now().year,
+        year=dt.now().year,
         message='Rendezvous With Me was created in 36 hours by a team of Rose-Hulman Students.'
     )
+
+@app.route('/schedule/<schedule_num>/timesluts', methods=['GET'])
+def timeslots(schedule_num):
+    schedule = []
+    for i in range(0, 10):
+        schedule.append({"date":dt.today(), "time": dt.utcnow()+datetime.timedelta(minutes=i), "duration":1,"open":1, "email":None})
+    return render_template('timeslot.html', title="Register For Timeslut", year = dt.now().year, items = schedule)
 
 @app.route('/schedule/create', methods=['POST'])
 def schedule():
     data = request.data
     client = pymongo.MongoClient("mongodb://Boilermake:Boilermake2017@cluster0-shard-00-00-sifbq.mongodb.net:27017,cluster0-shard-00-01-sifbq.mongodb.net:27017,cluster0-shard-00-02-sifbq.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
     db = client.time_slots
-    base = datetime.today()
+    base = dt.today()
     
     for date in range(data['start_date'].toordinal(), data['end_date'].toordinal()):
         for i in range(data["start_time"], data["end_time"], data["duration"]):
             time_slot = {"date":datetime.date.fromordinal(date), "time": i, "duration":data['duration'], "open":1, "email":None}
             db.insert_one(time_slot).inserted_id
-    return render_template('sendSchedule.html', title='Send Schedule', year=datetime.now().year, schedule_num = )
+    schedule_num = client.schedule.insert_one(schedule).inserted_id
+    return render_template('sendSchedule.html', title='Send Schedule', year=datetime.now().year, schedule_num = schedule_num)
