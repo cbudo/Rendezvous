03232a4b2acc6aa8e481c0106d5ff129f3745b9a
Christopher Budo <budocf@rose-hulman.edu>
2017-01-22 05:26:06 -0500

    got the db code a lot closer to working

--- Rendezvous/Rendezvous/views.py
+++ Rendezvous/Rendezvous/views.py
@@ -4,7 +4,23 @@ Routes and views for the flask application.
 import datetime
 from datetime import datetime as dt
 from flask import render_template
+from flask import request
 from Rendezvous import app
+import pymongo
+import json
+
+class TimeSlot:
+    date=dt.now().day
+    time=dt.now().time
+    duration=15
+    open=1
+    email=None
+    def __init__(self, date, time, duration, open, email):
+        self.date = date
+        self.time = time
+        self.duration = duration
+        self.open = open
+        self.email = email
 
 @app.route('/')
 @app.route('/home')
@@ -47,7 +63,8 @@ def about():
     )
 
 @app.route('/schedule/<schedule_num>/timesluts', methods=['GET'])
-def timeslots(schedule_num):
+def timeslots(schedule_num):    
+    client = pymongo.MongoClient("mongodb://Boilermake:Boilermake2017@cluster0-shard-00-00-sifbq.mongodb.net:27017,cluster0-shard-00-01-sifbq.mongodb.net:27017,cluster0-shard-00-02-sifbq.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
     schedule = []
     for i in range(0, 10):
         schedule.append({"date":dt.today(), "time": dt.utcnow()+datetime.timedelta(minutes=i), "duration":1,"open":1, "email":None})
@@ -55,14 +72,22 @@ def timeslots(schedule_num):
 
 @app.route('/schedule/create', methods=['POST'])
 def schedule():
-    data = request.data
+    data = request.form
     client = pymongo.MongoClient("mongodb://Boilermake:Boilermake2017@cluster0-shard-00-00-sifbq.mongodb.net:27017,cluster0-shard-00-01-sifbq.mongodb.net:27017,cluster0-shard-00-02-sifbq.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
-    db = client.time_slots
+    db = client.rendezvous
     base = dt.today()
     
-    for date in range(data['start_date'].toordinal(), data['end_date'].toordinal()):
-        for i in range(data["start_time"], data["end_time"], data["duration"]):
-            time_slot = {"date":datetime.date.fromordinal(date), "time": i, "duration":data['duration'], "open":1, "email":None}
-            db.insert_one(time_slot).inserted_id
-    schedule_num = client.schedule.insert_one(schedule).inserted_id
+    for date in daterange(dt.strptime(data['start_date'], "%m/%d/%Y"),dt.strptime(data['end_date'], "%m/%d/%Y")):
+        start = dt.strptime(data["start_time"], "%H:%M")
+        end = dt.strptime(data["end_time"], "%H:%M")
+        while start <= end:
+            time_slot = TimeSlot(str(date), str(start), data['slot_duration'], 1, None)
+            db.time_slots.insert_many(time_slot).inserted_id
+            start+=datetime.timedelta(minutes=int(data['slot_duration']))
+
+    schedule_num = db.schedule.insert_one(schedule).inserted_id
     return render_template('sendSchedule.html', title='Send Schedule', year=datetime.now().year, schedule_num = schedule_num)
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + datetime.timedelta(n)
