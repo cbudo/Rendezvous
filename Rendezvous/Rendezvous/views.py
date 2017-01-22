e14e1d0d56f47f1697fa549147013d6f3d390cc5
Christopher Budo <budocf@rose-hulman.edu>
2017-01-22 01:52:36 -0500

    added a page to send schedule fill in requests over email

--- Rendezvous/Rendezvous/views.py
+++ Rendezvous/Rendezvous/views.py
@@ -51,8 +51,10 @@ def schedule():
     data = request.data
     client = pymongo.MongoClient("mongodb://Boilermake:Boilermake2017@cluster0-shard-00-00-sifbq.mongodb.net:27017,cluster0-shard-00-01-sifbq.mongodb.net:27017,cluster0-shard-00-02-sifbq.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
     db = client.time_slots
-    for date in range(data['start_date'], data['end_date']):
+    base = datetime.today()
+    
+    for date in range(data['start_date'].toordinal(), data['end_date'].toordinal()):
         for i in range(data["start_time"], data["end_time"], data["duration"]):
-            time_slot = {"date":date, "time": i, "duration":data['duration'], "open":1, "email":None}
-            db.insert_one(time_slot)
-    return render_template('sendSchedule.html', title='Send Schedule', bytearray=datetime.now().year)
+            time_slot = {"date":datetime.date.fromordinal(date), "time": i, "duration":data['duration'], "open":1, "email":None}
+            db.insert_one(time_slot).inserted_id
+    return render_template('sendSchedule.html', title='Send Schedule', year=datetime.now().year, schedule_num = )
